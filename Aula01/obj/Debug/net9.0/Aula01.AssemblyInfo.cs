b0b6fc6971f95936efb3ebf71a9c47cd8ea1d249
jonatanVAMS <jonatanmoreira304@gmail.com>
2025-02-25 10:58:43 -0300

    Atividade01

--- Aula01/obj/Debug/net9.0/Aula01.AssemblyInfo.cs
+++ Aula01/obj/Debug/net9.0/Aula01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b4da1828cbfe1583fd9a0d1cbf868bc8c1648e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
